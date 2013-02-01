377f0ecf3ef8449d9c05937f7ffc4dbd96b861d1
MaxguN <florent.fortat@maxgun.fr>
2013-02-01 12:08:29 +0100

    Implemented service management

--- src/service/service.py
+++ src/service/service.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python
 
+import sys
 from ClusterShell.Task import task_self, NodeSet
 
-task = task_self()
-task.run("uname -r", nodes="node[1-8]")
+service = ''
+action = ''
+nodes = ''
 
-for output, nodes in task.iter_buffers():
-    print NodeSet.fromlist(nodes), output
+if len(sys.argv) == 4:
+	service = sys.argv[1]
+	action = sys.argv[2]
+	nodes = sys.argv[3]
+else:
+	print 'Usage : ' + sys.argv[0] + ' <service> <action> <node_list>'
+	exit(0)
+
+task = task_self()
+task.run('service ' + service + ' ' + action, nodes=nodes)
+
+for output, nodeset in task.iter_buffers():
+	# for node in nodeset:
+	# 	print node, output
+	print NodeSet.fromlist(nodeset), output
